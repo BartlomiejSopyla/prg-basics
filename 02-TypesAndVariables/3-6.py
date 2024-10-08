f8311a09b0d19caae023e55ccf43c8b61636dcd0
Bartlomiej Sopyla <bartlomiejsopyla.ti@gmail.com>
2024-10-08 13:47:08 +0200

    Sekcja 3 koniec

---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,7 @@
+import math
+h = 1.75
+h = float(h)
+d = 3.57*math.sqrt(h)
+d2= 3.57*math.sqrt(20)
+print('the distance to the horizon in kilometers', d)
+print('the distance to the horizon in kilometers', d2)
