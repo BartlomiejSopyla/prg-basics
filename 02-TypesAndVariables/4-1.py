5e4839a0861e87f6725678e69b29ba0dba8fe93f
Bartlomiej Sopyla <bartlomiejsopyla.ti@gmail.com>
2024-10-08 14:15:13 +0200

    Sekcja 4 koniec

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,9 @@
+###
+# Displaying student's personal data
+#
+name = "Adam"
+age = 19
+height = 180
+print(f"My name is {name}.\n")
+print(f"I am {age} years old, and my height is {height} cm.\n")
+print(f"In 6 years I will be {age+6} years old.")
