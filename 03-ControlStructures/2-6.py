09af2bedfbc4193383e34777bf1d7bfe057b5524
Bartlomiej Sopyla <bartlomiejsopyla.ti@gmail.com>
2024-10-15 13:13:30 +0200

    Sekcja 2 koniec

---
+++ 03-ControlStructures/2-6.py
@@ -0,0 +1,7 @@
+number = int(input('Numbr: '))
+if number>0:
+    print(f'Number {number} is positive')
+elif number<0:
+    print(f'Number {number} is negative')
+else:
+    print(f'Number {number} is zero')
