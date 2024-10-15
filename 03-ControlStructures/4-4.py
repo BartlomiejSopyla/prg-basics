43f9db0fffb9abc82ef05c1dec4808ad876d31c2
Bartlomiej Sopyla <bartlomiejsopyla.ti@gmail.com>
2024-10-15 17:43:36 +0200

    Sekcja 4 koniec

--- 03-ControlStructures/4-4.py
+++ 03-ControlStructures/4-4.py
@@ -3,9 +3,23 @@
 # with an extra space between characters (add a space between
 # each character)
 #
+# university = 'Krakow University of Economics'
+# university_expanded = ''
+
+# for char in university:
+#     print(char, end=' ')
+
+###
+# Prints the name of university where you are studying
+# with an extra space between characters (add a space between
+# each character)
+#
 university = 'Krakow University of Economics'
 university_expanded = ''
 
 for char in university:
-    print(char, end=' ')
+    university_expanded = university_expanded + char + ' '
+
+print(university_expanded)
+print(university_expanded.strip())  # Removes the trailing space if not desired
 
--- 03-ControlStructures/4-5.py
+++ 03-ControlStructures/4-5.py
@@ -6,15 +6,14 @@ plain_text = 'The early bird catches the worm'
 encrypted_text = ''
 
 for char in plain_text:
-    char = ord(plain_text)
+    char = ord(char)
     char = char+1
     # add one to the character's code
-    plain_text=char
+    char = chr(char)
     # replace new character code with its
     # corresponding character (use chr())
-    ...
+    encrypted_text += char
     # add encrypted character to encrypted text
-    ...
 
-print(plain_text...)
-print(encrypted_text...)
+print(plain_text)
+print(encrypted_text)
