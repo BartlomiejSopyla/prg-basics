f9d529d426429c0e6f43718162e85338f9940b8a
Bartlomiej Sopyla <bartlomiejsopyla.ti@gmail.com>
2024-10-23 21:22:18 +0200

    sekcja 7 do 6

---
+++ 04-Functions/7-6.py
@@ -0,0 +1,10 @@
+import binary
+
+number1 = '101101'
+number2 = '1311a10100'
+
+result = binary.f(number1)
+result2 = binary.f(number2)
+
+print(f'Is {number1} a binary number: {result}')
+print(f'Is {number2} a binary number: {result2}')
